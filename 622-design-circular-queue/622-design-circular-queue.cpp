4cd90cbfc50aa87655ac182c74c3e66db0901fc2
Parth-2000 <67038867+Parth-2000@users.noreply.github.com>
2022-09-25 13:35:15 +0530

    Time: 29 ms (81.23%), Space: 17.1 MB (11.06%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -1,47 +1,46 @@
-class MyCircularQueue {
-public:
-    int head = 0;
-    int tail = -1;
-    int maxSize;
-    vector<int> data;
-    MyCircularQueue(int k) {
-        maxSize = k;
-        data.resize(k);
-    }
-    
-    bool enQueue(int val) {
-        if (isFull()) return false;
-        tail = (tail + 1) % maxSize;
-        data[tail] = val;
-        return true;
-    }
-    bool deQueue() {
-        if (isEmpty()) return false;
-        if (head == tail) head = 0, tail = -1;
-        else head = (head + 1) % maxSize;
-        return true;
-    }
-    int Front() {
-        return isEmpty() ? -1 : data[head];
-    }
-    int Rear() {
-        return isEmpty() ? -1 : data[tail];
-    }
-    bool isEmpty() {
-        return tail == -1;
-    }
-    bool isFull() {
-        return !isEmpty() && (tail + 1) % maxSize == head;
-    }
+struct Node {
+    public:
+        int val;
+        Node* next;
+        Node(int v, Node* n=nullptr) {
+            val = v;
+            next = n;
+        }
 };
+class MyCircularQueue {
+    public:
+        int maxSize, size = 0;
+        Node *head = new Node(0), *tail = new Node(0);
+        MyCircularQueue(int k) {
+            maxSize = k;
+        }
+        bool enQueue(int val) {
+            if (isFull()) return false;
+            Node* newNode = new Node(val);
+            if (isEmpty()) head = newNode, tail = newNode;
+            else tail->next = newNode, tail = tail->next;
+            size++;
+            return true;
+        }
+        bool deQueue() {
+            if (isEmpty()) return false;
+            Node* rem = head;
+            head = head->next;
+            delete rem;
+            size--;
+            return true;
+        }
+        int Front() {
+            return isEmpty() ? -1 : head->val;
+        }
+        int Rear() {
+            return isEmpty() ? -1 : tail->val;
+        }
+        bool isEmpty() {
+            return size == 0;
+        }
+        bool isFull() {
+            return size == maxSize;
+        }
 
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
+};
